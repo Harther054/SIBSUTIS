1258fe91a79ff93e0ae00a1038c4b2675df5060e
fgd <140924114+Harther054@users.noreply.github.com>
2024-03-04 05:11:45 +0700

    Add files via upload

---
+++ Курс3/Семестр 5/FLP/RGZ1.pl
@@ -0,0 +1,16 @@
+%Базовый случай: пустой список или сдвиг на 0 элементов не изменяет
+%список.
+shift_left(List, 0, List) :- !.
+shift_left([], _, []) :- !.
+
+% Правило для сдвига списка на N элементов влево
+shift_left([Head|Tail], N, Result) :-
+    N > 0,
+    append(Tail, [Head], Shifted),
+    N1 is N - 1,
+    shift_left(Shifted, N1, Result).
+shift_left(List, 0, List).
+shift_left(List, N, Result) :-
+    length(List, Length),
+    N1 is N mod Length,
+    shift_left(List, N1, Result).
